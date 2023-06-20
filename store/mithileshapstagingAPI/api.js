8af51da18b17e8b7eda3f02f26d0cb2c34fc6398
Mithilesh Tiwari <Mithilesh@crowdbotics.com>
2023-06-20 15:12:26 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/mithileshapstagingAPI/api.js
+++ store/mithileshapstagingAPI/api.js
@@ -1,58 +1,72 @@
-import axios from "axios"
+import axios from "axios";
 const mithileshapstagingAPI = axios.create({
   baseURL: "https://mithileshapstaging--93355.botics.co",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function api_docs_schema_retrieve(payload) {
   return mithileshapstagingAPI.get(`/api-docs/schema/`, {
-    params: { lang: payload.lang }
-  })
+    params: {
+      lang: payload.lang
+    }
+  });
 }
+
 function api_v1_login_create(payload) {
-  return mithileshapstagingAPI.post(`/api/v1/login/`, payload.data)
+  return mithileshapstagingAPI.post(`/api/v1/login/`, payload.data);
 }
+
 function api_v1_signup_create(payload) {
-  return mithileshapstagingAPI.post(`/api/v1/signup/`, payload.data)
+  return mithileshapstagingAPI.post(`/api/v1/signup/`, payload.data);
 }
+
 function rest_auth_login_create(payload) {
-  return mithileshapstagingAPI.post(`/rest-auth/login/`, payload.data)
+  return mithileshapstagingAPI.post(`/rest-auth/login/`, payload.data);
 }
+
 function rest_auth_logout_retrieve(payload) {
-  return mithileshapstagingAPI.get(`/rest-auth/logout/`)
+  return mithileshapstagingAPI.get(`/rest-auth/logout/`);
 }
+
 function rest_auth_logout_create(payload) {
-  return mithileshapstagingAPI.post(`/rest-auth/logout/`)
+  return mithileshapstagingAPI.post(`/rest-auth/logout/`);
 }
+
 function rest_auth_password_change_create(payload) {
-  return mithileshapstagingAPI.post(`/rest-auth/password/change/`, payload.data)
+  return mithileshapstagingAPI.post(`/rest-auth/password/change/`, payload.data);
 }
+
 function rest_auth_password_reset_create(payload) {
-  return mithileshapstagingAPI.post(`/rest-auth/password/reset/`, payload.data)
+  return mithileshapstagingAPI.post(`/rest-auth/password/reset/`, payload.data);
 }
+
 function rest_auth_password_reset_confirm_create(payload) {
-  return mithileshapstagingAPI.post(
-    `/rest-auth/password/reset/confirm/`,
-    payload.data
-  )
+  return mithileshapstagingAPI.post(`/rest-auth/password/reset/confirm/`, payload.data);
 }
+
 function rest_auth_registration_create(payload) {
-  return mithileshapstagingAPI.post(`/rest-auth/registration/`, payload.data)
+  return mithileshapstagingAPI.post(`/rest-auth/registration/`, payload.data);
 }
+
 function rest_auth_registration_verify_email_create(payload) {
-  return mithileshapstagingAPI.post(
-    `/rest-auth/registration/verify-email/`,
-    payload.data
-  )
+  return mithileshapstagingAPI.post(`/rest-auth/registration/verify-email/`, payload.data);
 }
+
 function rest_auth_user_retrieve(payload) {
-  return mithileshapstagingAPI.get(`/rest-auth/user/`)
+  return mithileshapstagingAPI.get(`/rest-auth/user/`);
 }
+
 function rest_auth_user_update(payload) {
-  return mithileshapstagingAPI.put(`/rest-auth/user/`, payload.data)
+  return mithileshapstagingAPI.put(`/rest-auth/user/`, payload.data);
 }
+
 function rest_auth_user_partial_update(payload) {
-  return mithileshapstagingAPI.patch(`/rest-auth/user/`, payload.data)
+  return mithileshapstagingAPI.patch(`/rest-auth/user/`, payload.data);
 }
+
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -68,4 +82,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-}
+};
